9c8b76a8300ac2f7e26fa030a6c6bbb8d9bd12b8
Muhammad Suleman <88381507+supolaris@users.noreply.github.com>
2023-05-11 13:01:29 +0500

    update startup.js

---
+++ src/screens/startUpScreen/startUp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {View, Text, SafeAreaView} from "react-native";
+
+
+export default function StartUp() {
+
+    return(
+        <SafeAreaView>
+            <View>
+                
+            </View>
+
+        </SafeAreaView>
+    )
+}
